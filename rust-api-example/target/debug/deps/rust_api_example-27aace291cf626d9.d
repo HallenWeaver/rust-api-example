96b0cb5ae7191df8b5766caae9e531bbc7f4bc4e
HallenWeaver <alexandre.rmiquilino@gmail.com>
2023-02-17 22:47:59 -0300

    Hello, world! Works on Rust

---
+++ rust-api-example/target/debug/deps/rust_api_example-27aace291cf626d9.d
@@ -0,0 +1,5 @@
+/home/alexandre/APIs/rust-api-example/rust-api-example/target/debug/deps/rust_api_example-27aace291cf626d9.rmeta: src/main.rs
+
+/home/alexandre/APIs/rust-api-example/rust-api-example/target/debug/deps/rust_api_example-27aace291cf626d9.d: src/main.rs
+
+src/main.rs:
---
+++ rust-api-example/target/debug/deps/rust_api_example-d6feb64d61871336.d
@@ -0,0 +1,5 @@
+/home/alexandre/APIs/rust-api-example/rust-api-example/target/debug/deps/rust_api_example-d6feb64d61871336.rmeta: src/main.rs
+
+/home/alexandre/APIs/rust-api-example/rust-api-example/target/debug/deps/rust_api_example-d6feb64d61871336.d: src/main.rs
+
+src/main.rs:
