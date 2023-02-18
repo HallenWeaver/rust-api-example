01c39e0ebfc449e167d3b4d4d0264fa77b37893b
HallenWeaver <alexandre.rmiquilino@gmail.com>
2023-02-17 23:32:29 -0300

    Removed wrong commit

--- rust-api-example/target/debug/deps/rust_api_example-d6feb64d61871336.d
+++
@@ -1,5 +0,0 @@
-/home/alexandre/APIs/rust-api-example/rust-api-example/target/debug/deps/rust_api_example-d6feb64d61871336.rmeta: src/main.rs
-
-/home/alexandre/APIs/rust-api-example/rust-api-example/target/debug/deps/rust_api_example-d6feb64d61871336.d: src/main.rs
-
-src/main.rs:
